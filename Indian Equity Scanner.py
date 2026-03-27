da056aa398f9d36becdbb898a013ace6a277144d
Senyu <83979333+senyufrr@users.noreply.github.com>
2026-03-27 10:54:31 +0530

    Indian Equity Scanner.py

--- Indian Equity Scanner.py
+++ Indian Equity Scanner.py
@@ -1,25 +1,20 @@
 import alpaca_trade_api as tradeapi
 
-# 1. Put your Alpaca keys here (Keep the single quote marks around them!)
 API_KEY =  -------------------
 SECRET_KEY = -----------------
 
-# 2. Tell the bot to use the Paper Trading (fake money) URL
 BASE_URL = 'https://paper-api.alpaca.markets'
 
 print("Attempting to connect to the stock market...")
 
 try:
-    # 3. Establish the connection
     api = tradeapi.REST(API_KEY, SECRET_KEY, BASE_URL, api_version='v2')
 
-    # 4. Check our bank account
     account = api.get_account()
     print(f"SUCCESS! Connection established.")
     print(f"Account Status: {account.status}")
     print(f"My Fake Buying Power: ${account.buying_power}")
     
-    # Let's pull the live price of Apple stock just to prove it works
     ticker = 'AAPL'
     quote = api.get_latest_quote(ticker)
     print(f"\nLive Data for {ticker}:")
